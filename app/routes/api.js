c774a6728751d7531d8590b893ce21e3e58a59a2
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2015-10-21 20:40:04 +0100

    use express jwt instead of vanilla

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,6 +1,7 @@
 var User       = require('../models/user');
 var jwt        = require('jsonwebtoken');
 var config     = require('../../config');
+const expressJwt = require('express-jwt');
 
 // super secret for creating tokens
 var superSecret = config.secret;
@@ -100,44 +101,9 @@ module.exports = function(app, express) {
         });
     });
 
-    // route middleware to verify a token
-    apiRouter.use(function(req, res, next) {
-        // do logging
-        console.log('Somebody just came to our app!');
-
-        // check header or url parameters or post parameters for token
-        var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-        // decode token
-        if (token) {
-
-            // verifies secret and checks exp
-            jwt.verify(token, superSecret, function(err, decoded) {
-
-                if (err) {
-                    res.status(403).send({
-                        success: false,
-                        message: 'Failed to authenticate token.'
-                    });
-                } else {
-                    // if everything is good, save to request for use in other routes
-                    req.decoded = decoded;
-
-                    next(); // make sure we go to the next routes and don't stop here
-                }
-            });
-
-        } else {
-
-            // if there is no token
-            // return an HTTP response of 403 (access forbidden) and an error message
-            res.status(403).send({
-                success: false,
-                message: 'No token provided.'
-            });
-
-        }
-    });
+    apiRouter.use(expressJwt({
+        secret: config.secret,
+    }));
 
     // test route to make sure everything is working
     // accessed at GET http://localhost:8080/api

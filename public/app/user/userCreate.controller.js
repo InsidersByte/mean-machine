52f99a7420909778392fc2ebef58c8a357341cd6
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2015-10-22 11:47:00 +0100

    refactored user controllers

---
+++ public/app/user/userCreate.controller.js
@@ -0,0 +1,41 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.user')
+        .controller('userCreateController', userCreate);
+
+    userCreate.$inject = ['User'];
+
+    function userCreate(User) {
+        /*jshint validthis:true */
+        const vm = this;
+
+        // variable to hide/show elements of the view
+        // differentiates between create or edit pages
+        vm.type = 'create';
+
+        vm.processing = false;
+        vm.message = '';
+        vm.userData = {};
+        vm.saveUser = saveUser;
+
+        ////////////////////
+
+        // function to create a user
+        function saveUser() {
+            vm.processing = true;
+            vm.message = '';
+
+            // use the create function in the userService
+            User
+                .create(vm.userData)
+                .success(function(data) {
+                    vm.processing = false;
+                    vm.userData = {};
+                    vm.message = data.message;
+                });
+
+        };
+    }
+}());
---
+++ public/app/user/userEdit.controller.js
@@ -0,0 +1,52 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.user')
+        .controller('userEditController', userEdit);
+
+    userEdit.$inject = ['User', '$routeParams'];
+
+    function userEdit(User, $routeParams) {
+        /*jshint validthis:true */
+        const vm = this;
+
+        // variable to hide/show elements of the view
+        // differentiates between create or edit pages
+        vm.type = 'edit';
+
+        vm.processing = false;
+        vm.message = '';
+        vm.saveUser = saveUser;
+
+        activate();
+
+        ////////////////////
+
+        function activate() {
+            User
+                .get($routeParams.user_id)
+                .success(function(data) {
+                    vm.userData = data;
+                });
+        }
+
+        // function to save the user
+        function saveUser() {
+            vm.processing = true;
+            vm.message = '';
+
+            // call the userService function to update
+            User.update($routeParams.user_id, vm.userData)
+                .success(function(data) {
+                    vm.processing = false;
+
+                    // clear the form
+                    vm.userData = {};
+
+                    // bind the message from our API to vm.message
+                    vm.message = data.message;
+                });
+        }
+    }
+}());

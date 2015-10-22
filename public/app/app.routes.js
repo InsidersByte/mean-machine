16718ac820a1cbc42939b7e1fabdcf68d48a769c
Jonathon Kelly <jonathon.kelly@insidersbyte.com>
2015-10-22 11:48:55 +0100

    refactored pages to the user module

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -6,36 +6,36 @@ angular.module('app.routes', ['ngRoute'])
 
             // route for the home page
             .when('/', {
-                templateUrl : 'app/views/pages/home.html'
+                templateUrl: 'app/views/pages/home.html',
             })
 
             // login page
             .when('/login', {
-                templateUrl : 'app/views/pages/login.html',
-                controller  : 'mainController',
-                controllerAs: 'login'
+                templateUrl: 'app/views/pages/login.html',
+                controller: 'mainController',
+                controllerAs: 'login',
             })
 
             // show all users
             .when('/users', {
-                templateUrl: 'app/views/pages/users/all.html',
+                templateUrl: 'app/user/all.html',
                 controller: 'userController',
-                controllerAs: 'user'
+                controllerAs: 'user',
             })
 
             // form to create a new user
             // same view as edit page
             .when('/users/create', {
-                templateUrl: 'app/views/pages/users/single.html',
+                templateUrl: 'app/user/single.html',
                 controller: 'userCreateController',
-                controllerAs: 'user'
+                controllerAs: 'user',
             })
 
             // page to edit a user
             .when('/users/:user_id', {
-                templateUrl: 'app/views/pages/users/single.html',
+                templateUrl: 'app/user/single.html',
                 controller: 'userEditController',
-                controllerAs: 'user'
+                controllerAs: 'user',
             });
 
         $locationProvider.html5Mode(true);
